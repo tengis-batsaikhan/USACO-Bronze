e9782bc4c52d7fafa94ed26df5f99a041c3c01b5
tengis-batsaikhan <126446746+tengis-batsaikhan@users.noreply.github.com>
2023-02-27 14:52:29 +0800

    Update The Cow-Signal.cpp

--- The Cow-Signal.cpp
+++ The Cow-Signal.cpp
@@ -3,26 +3,49 @@
 using namespace std;
 
 int main() {
-	int i, j, n, m, l = 0, x;
+	int i, j, h, n, m, l = 0, x, y;
 
-	string str;
+	string str, str2;
 
-	cin >> n >> x;
+	cin >> n >> x >> y;
+	
+	
+	
+	for(i = 0; i < x*y*n; i++){
+		str2[i] = 'i';
+	}
+	
+	
+
 
 	while(l < x) {
 		
 		cin >> str;
 		
-		for(j = 0; j < n; j++) {
-			for(i = 0; i <= str.size(); i++) {
-				for(m = 0; m < n; m++) {
-					cout << str[i];
+		for(h = 0; h < str2.size(); h++) {
+			for(j = 0; j < n; j++) {
+				for(i = 0; i <= str.size(); i++) {
+					for(m = 0; m < n; m++) {
+//					cout << str[i];
+						str2[h] = str[i];
+					}
 				}
+				cout << endl;
 			}
-			cout << endl;
+
 		}
 		l++;
 	}
+	
+	
+	for(i = 0; i <= str2.size(); i++){
+		cout << str2[i];
+	}
+
+
+
+
+
 
 
 
@@ -32,4 +55,3 @@ int main() {
 
 	return 0;
 }
-
